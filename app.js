e75c8d3d26c66326d7b8ce738cc5e682db545414
clementtan99 <clementtan999@gmail.com>
2025-07-30 11:17:50 +0800

    editProfile change to editprofile

--- app.js
+++ app.js
@@ -321,7 +321,7 @@ app.get('/profile', checkAuthenticated, (req, res) => {
 });
 
 app.get('/edit-profile', checkAuthenticated, (req, res) => {
-    res.render('editProfile', { user: req.session.user });
+    res.render('editprofile', { user: req.session.user });
 });
 
 app.post('/edit-profile', (req, res) => {
